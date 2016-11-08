e3dd5302fd86da8765a1890382915ce44a762325
Alex Doundakov <adoundakov@gmail.com>
2016-11-07 18:45:47 -0800

    flesh out company detail page, links not working

--- frontend/components/companies/detail/company_overview.jsx
+++ frontend/components/companies/detail/company_overview.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const CompanyOverview = (props) => {
+const CompanyOverview = ({company}) => {
   return (
     <div className='company-overview'>
-      <div>COMPANY OVERVIEW</div>
+      <h3>{company.name} Overview</h3>
+      <div className='overview-table'>
+        <div className='row'>
+          <label className='key'>Website</label>
+          <span className='value'>{company.company_url}</span>
+        </div>
+        <div className='row'>
+          <label className='key'>Size</label>
+          <span className='value'>{company.size}</span>
+        </div>
+        <div className='row'>
+          <label className='key'>Headquarters</label>
+          <span className='value'>{company.location}</span>
+        </div>
+        <div className='row'>
+          <label className='key'>CEO</label>
+          <span className='value'>{company.ceo}</span>
+        </div>
+      </div>
     </div>
   );
 };
