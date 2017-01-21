7b77e66141e8f5301c57d20c9741640342dad7a0
Alex Doundakov <adoundakov@gmail.com>
2017-01-20 21:54:27 -0800

    Remove react-logger

--- frontend/middleware/root_middleware.js
+++ frontend/middleware/root_middleware.js
@@ -4,12 +4,8 @@ import PostingMiddleware from './posting_middleware';
 import CompanyMiddleware from './company_middleware';
 import ApplicationMiddleware from './application_middleware';
 import ReviewMiddleware from './review_middleware';
-import createLogger from 'redux-logger';
-
-const LoggerMiddleware = createLogger();
 
 const RootMiddleware = applyMiddleware(
-  LoggerMiddleware,
   SessionMiddleware,
   PostingMiddleware,
   CompanyMiddleware,
