5a3857aac5ccbfe7d0cedb2bbe90ca21b1925aa0
Alex Doundakov <adoundakov@gmail.com>
2017-01-22 15:42:13 -0800

    self host logo urls

--- frontend/easyhire.jsx
+++ frontend/easyhire.jsx
@@ -13,10 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
 
-  // TODO: Remove testing code
-  window.store = store;
-  // end testing code
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
